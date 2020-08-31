ef0de1d8caed7fe90e86fd6aa00c1f7bd5ad576d
Karen Sofia <ramireztovarkaren@gmail.com>
2020-08-31 02:43:53 -0500

    Funcionan los botones de + y -

--- javaScript/JavaScript.js
+++ javaScript/JavaScript.js
@@ -1,5 +1,17 @@
-﻿const input = document.getElementById(entrada);
+﻿const botonAdivinarNumero = document.getElementById("boton_adivinar_numero");
+const sumar = document.getElementById("sum");
+const suposicionNumero = document.getElementById("suposicion_numero");
+const restar = document.getElementById("res");
 
-input.addEventListener("click", function actualizar(){
+let numUser = 0;
 
-});
+function actualizarNumero(operadorId) {
+
+    if (operadorId === "sum" && numUser <= 9) {
+        document.getElementById("suposicion_numero").innerText = ++numUser;
+    }
+    if (operadorId === "res" && numUser >= 1) {
+        document.getElementById("suposicion_numero").innerText = --numUser;
+    }
+}
+
