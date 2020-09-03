a33262d2662409e831a4734469cc418919c38ec2
Karen Sofia <ramireztovarkaren@gmail.com>
2020-09-02 20:56:20 -0500

    Genera un gandor y no permite seguir jugando

--- javaScript/JavaScript.js
+++ javaScript/JavaScript.js
@@ -1,14 +1,17 @@
-﻿const botonAdivinarNumero = document.getElementById("adivina_numero");
+﻿let botonAdivinarNumero = document.getElementById("adivina_numero");
+const siguienteRonda = document.getElementById("siguiente_ronda");
 
-let suposicionComputadora = document.getElementById("suposicion_computadora")
-
+let suposicionComputadora = document.getElementById("suposicion_computadora");
 let suposicionUsuario = document.getElementById("suposicion_usuario");
 let numeroClave = document.getElementById("numero_clave");
+
 let numUser = 0;
+let numUserPuntaje = 0;
+let numComputadoraPuntaje = 0;
 
 
 function actualizarNumero(operadorId) {
-
+    
     if (operadorId === "sum" && numUser <= 9) {
         suposicionUsuario.innerText = ++numUser;
     }
@@ -16,34 +19,66 @@ function actualizarNumero(operadorId) {
         suposicionUsuario.innerText = --numUser;
     }
 }
-alert("c1");
+
+
 const generarGanador = (numeroUser, numeroComputadora, objetivoNumero) => {
 
     let diferenciaUsuario = Math.abs(objetivoNumero - numeroUser);
     let diferenciaComputadora = Math.abs(objetivoNumero - numeroComputadora);
 
+    let resultadoComputadora = document.getElementById("resultado_computadora");
+    let puntajeComputadora = document.getElementById("puntaje_computadora");
+    let puntajeUsuario = document.getElementById("puntaje_usuario");
+
+
     if (diferenciaComputadora > diferenciaUsuario) {
+
         botonAdivinarNumero.innerText = "Ganaste!!!";
+        puntajeUsuario.innerText = "Puntaje: " + ++numUserPuntaje;
+        botonAdivinarNumero.style.backgroundColor = "gainsboro";
     }
     else {
-        resutadoComputadora.innerHTML = "La Computadora Gano!!!";
+        resultadoComputadora.innerText = "La Computadora Gano!!!";
+        puntajeComputadora.innerText = "Puntaje" + ++numComputadoraPuntaje;
+        botonAdivinarNumero.style.backgroundColor = "gainsboro";
     }
 }
 
-alert("d2");
+
+
 const generarNumero = () => {
 
     let generarNumeroClave = Math.floor(Math.random() * 10);
     let generarNumeroComputadora = Math.floor(Math.random() * 10);
-    let resultadoComputadora = document.getElementById("resultado_computadora");
 
-    suposicionUsuario = document.getElementById("suposicion_usuario");
+    suposicionUsuario = document.getElementById("suposicion_usuario").innerText;
 
-    numeroClave.innerText = generarNumeroClave;
-    suposicionComputadora.innerText = generarNumeroComputadora;
+    
+    if (!(document.getElementById("adivina_numero").innerText === "Ganaste!!!" ||
+        document.getElementById("resultado_computadora").innerText === "La Computadora Gano!!!")) {
 
-    generarGanador(suposicionUsuario, suposicionComputadora, numeroClave);
+        numeroClave.innerText = "Numero Clave: " + generarNumeroClave;
+        suposicionComputadora.innerText = generarNumeroComputadora;
+
+        generarGanador(suposicionUsuario, generarNumeroComputadora, generarNumeroClave);
+
+    }
+    else {
+        botonAdivinarNumero.style.backgroundColor = "gainsboro";
+    }
 }
 
-alert("c3");
+
+const proximaRonda = () => {
+    suposicionUsuario = document.getElementById("suposicion_usuario");
+    alert("Proxima ronda");
+    alert(suposicionUsuario);
+    suposicionUsuario.innerText = 0;
+    alert(suposicionUsuario);
+}
+
+
 botonAdivinarNumero.addEventListener("click", generarNumero);
+alert("proxima ronda");
+siguienteRonda.addEventListener("click", proximaRonda);
+alert("proxima ronda");
